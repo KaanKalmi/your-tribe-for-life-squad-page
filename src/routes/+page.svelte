dff8ba19e739c473455b6d89826e8f57611ef1bc
koeenm <mkoeen2@gmail.com>
2024-09-13 14:32:29 +0200

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,97 @@
 <script>
-	export let data;
+  export let data;
 </script>
 
-<h1>FDND</h1>
+<body>
+  <h1>FDND</h1>
 
-<section>
-	<h2>{data.persons[1].name} {data.persons[1].prefix} {data.persons[1].surname}</h2>
+  <section>
+    <ul>
+      {#each data.persons as person}
+        <li>
+          <a href="/person/{person.id}">
+            <img src="{person.avatar}" alt="{person.name}">
+            <h2>{person.name} {person.prefix} {person.surname}</h2>
+            <p>{person.role}</p>
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </section>
+</body>
 
-	<img src="{data.persons[1].avatar}" alt="{data.persons[1].name}">
-</section>
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap');
+
+	* {
+		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+		height: 100%;
+		scroll-snap-type: x mandatory;
+		overflow-x: hidden;
+	}
+
+	h1 {
+		display: flex;
+		justify-content: center;
+		margin: 1em 0;
+		color: white;
+		/* transform: rotate(-1deg); */
+	}
+
+	body {
+		height: 100%;
+		width: 100%;
+		margin: 0;
+		background-color: #4D10CF;
+	}
+
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1.5em;
+		padding: 0;
+		margin: 0;
+
+		justify-content: center;
+	}
+
+	li {
+        display: flex;
+        flex-direction: column;
+		align-items: center;
+		max-height: 20em;
+        /* background-color: white; */
+        /* border: 0.1em solid black; */
+
+		list-style: none;
+		width: 12em;
+    } 
+
+	a {
+		text-decoration: none;
+		color: black	;
+	}
+
+    h2, p {
+        text-align: center;
+        font-family: Caveat, cursive;
+		color: white;
+		margin: 0;
+    }
+
+	p {
+		font-size: 1.5em;
+	}
+
+    img {
+        width: 12em;
+		height: 12em;
+		object-fit: cover;
+		border-radius: 50%;
+        /* margin: 1em; */
+        /* left: 0; */
+
+        /* transform: rotate(-1deg) */
+    }
+</style>
+
---
+++ src/routes/person/{id}/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+    export let data;
+  </script>
+
+<body>
+
+<header>
+    <a href="../">Terug</a>
+    <h1>Visitekaartje</h1>
+</header>
+
+<section>
+    <img src="{data.person.avatar}" alt="avatar">
+    <h2>{data.persons.name}</h2>
+    <p>{data.persons.bio}</p>
+</section>
+    
+</body>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap');
+
+    * {
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        height: 100%;
+    }
+
+    body {
+        height: 100%;
+        background-color: #4D10CF;
+    }
+
+    header {
+        display: flex;
+        align-items: center;
+        margin: 1em 0;
+
+        & a {
+            text-decoration: none;
+            color: black;
+            background-color: white;
+            border: 0.1em solid black;
+            padding: 1em;
+
+            border-radius: 50% 0 0 50%;
+        }
+
+        & h1 {
+            margin: 0 auto;
+            transform: rotate(1deg);
+            color: white;
+        }
+    }
+
+    section {
+        display: flex;
+        flex-direction: column;
+        background-color: white;
+        border: 0.1em solid black;
+    } 
+
+    @media (min-width: 720px) {
+        section {
+            max-width: 60vw;
+            margin: 0 auto;
+        }
+    }
+
+    h2, p {
+        text-align: center;
+        font-family: Caveat, cursive;
+    }
+
+    h2 {
+        font-size: 2em;
+        margin: 0;
+    }
+
+    img {
+        max-width: 10rem;
+        margin: 1em;
+        left: 0;
+
+        transform: rotate(-1deg)
+    }
+</style>
